52cc89822d1df631669767333eb43cdfb860ccaf
Nathanage3 <sisay954@gmail.com>
2024-06-04 19:39:20 +0300

    new update

--- playground/views.py
+++ playground/views.py
@@ -1,17 +1,43 @@
+from django.db import transaction
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Q, F
-from django.db.models.aggregates import Count, Min, Max, Avg, Sum
-from store.models import Product, OrderItem, Customer, Order
-
+from django.db.models import DecimalField, Q, F, Value, Func, Count, ExpressionWrapper
+from django.db.models.aggregates import Min, Max, Avg, Sum
+from store.models import Product, OrderItem, Customer, Order, Collection
+from tags.models import TaggedItem
 
 
 def say_hello(request):
- queryset =  Product.objects.aggregate(Count('id'))
- context = {
-        'name': 'Sisay',
-        'product': queryset
- }
- return render(request, 'hello.html', context)
+  #queryset = TaggedItem.objects.get_tags_for(Product, 1)
+  """queryset = Product.objects.all()
+  queryset[0] => caching
+  list(queryset)
+  """ 
+  #collection = Collection(title_isull=False)
+  """collection = Collection(pk=4)
+     collection.delete()
+  """
+
+  #collection.objects.create(title='Video Games', featured_product_id=1)
+  #Collection.objects.filter(pk=11).update(featured_product=None)
+  with transaction.atomic(): 
+   order = Order()
+   order.customer_id = 1
+   order.save()
+
+   item = OrderItem()
+   item.order = order
+   item.product_id = 1
+   item.quantity = 1
+   item.unit_price = 10
+   item.save()
+
+
+  
+
+
+
+  return render(request, 'hello.html')
  
